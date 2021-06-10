8f4eb04a246ff0e34e8372a43aacf8c1f20b40ce
kuntdutt <kuntdutt@cisco.com>
2021-06-10 13:42:25 +0530

    updated

--- src/main/java/com/example/customer/CustomerApplication.java
+++ src/main/java/com/example/customer/CustomerApplication.java
@@ -2,9 +2,8 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+
 @SpringBootApplication
 public class CustomerApplication {
 
